3feffa9a81ba9ce454ae1ffa4c09503ac1ec39ad
wudandan <wudandan@jiuqi.com.cn>
2020-10-26 10:13:32 +0800

    first commit

---
+++ src/common/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header">
+    <div class="headLeft">
+      <div class="logo"><img src="../assets/logo.png" alt="" /></div>
+      <div class="name">wodemingzi</div>
+    </div>
+    <div class="headRight">
+      <ul style="color: #fff; display: flex">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+      <!-- <div><span style="color: #fff">退出登录</span></div> -->
+    </div>
+  </div>
+</template>   
+
+<style scoped lang="less">
+.header {
+  display: flex;
+  justify-content: space-between;
+  .headLeft {
+    display: flex;
+
+    width: 300px;
+    .logo {
+      img {
+        height: 34px;
+        margin-top: 15px;
+      }
+    }
+    .name {
+      padding-left: 5px;
+      font-size: 22px;
+      color: #fff;
+      line-height: 64px;
+    }
+  }
+  .headRight {
+    width: 200px;
+    display: flex;
+  }
+}
+</style>
